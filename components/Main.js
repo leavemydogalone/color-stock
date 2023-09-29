64835fbb51d8a16eb6027b99512a4083002a530d
leavemydogalone <niklasport88@gmail.com>
2023-09-29 16:28:19 -0700

    added instructions

--- components/Main.js
+++ components/Main.js
@@ -1,17 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styles from "./Main.module.css";
 import ChartContainer from "./ChartContainer";
 import { ColorsContext } from "../utils/ColorsProvider";
+import PopUp from "./PopUp";
 
 export default function Main() {
   const colorsContextProvider = useContext(ColorsContext);
 
   const { colorsState } = colorsContextProvider;
   const { marketColors } = colorsState;
+  const [popUpVisible, setPopUpVisible] = useState(true);
 
   return (
     <main className={styles.main}>
       {marketColors ? <ChartContainer /> : "Loading"}
+      <PopUp popUpVisible={popUpVisible} setPopUpVisible={setPopUpVisible} />
+      <button className={styles.button} onClick={() => setPopUpVisible(true)}>
+        Show Instructions
+      </button>
     </main>
   );
 }
---
+++ components/PopUp.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./PopUp.module.css";
+import { useState } from "react";
+
+export default function PopUp({ popUpVisible, setPopUpVisible }) {
+  let display = popUpVisible ? "flex" : "none";
+  return (
+    <div style={{ display: display }} className={styles.popUp}>
+      <div
+        className={styles.background}
+        onClick={() => setPopUpVisible(false)}
+      ></div>
+      <section className={styles.popUpMain}>
+        <div className={styles.title}>
+          Instructions{" "}
+          <span className={styles.x} onClick={() => setPopUpVisible(false)}>
+            X
+          </span>
+        </div>
+        <p className={styles.subtitle}>
+          Welcome to the wonderful world of color trading!
+        </p>
+        <p className={styles.text}>
+          Along the top of the screen you can see the daily increase and
+          decrease in the market count of each stock. <br />
+          Trades happen daily with the help of our handy automated trading
+          helper that buys and sells colors every day around 4pm PST.
+        </p>
+        <p className={styles.text}>
+          In the center of the screen, you can see the current market count of
+          each color. This market is the same for every one who everyone who
+          visits, and so any action you take will affect the market for everyone
+          else.
+        </p>
+        <p className={styles.text}>
+          At the bottom, you can see your wallet of colors. You have been gifted
+          30 of each color.
+          <br />
+          Using the buy and sell sections for each color, you can buy and sell
+          the colors from and to the market in real time.
+        </p>
+      </section>
+    </div>
+  );
+}
